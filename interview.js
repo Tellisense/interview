9067ab5981c3de0f56f9ade38fcb2653f70ba8e9
Justin Rahimi <9245522+Tellisense@users.noreply.github.com>
2021-04-21 17:29:27 -0700

    added sorting Array

--- interview.js
+++ interview.js
@@ -321,3 +321,58 @@ function sockMerchant(n, ar) {
 undefined;
 sockMerchant(9, arr);
 3;
+
+//write a function, given an array of object, sort the array according to value inside the objects.
+
+var people = [
+  {
+    id: 1,
+    name: "Leanne Graham",
+    username: "Bret",
+    email: "Sincere@april.biz",
+    address: {
+      street: "Kulas Light",
+      suite: "Apt. 556",
+      city: "Gwenborough",
+      zipcode: "92998-3874",
+      geo: {
+        lat: "-37.3159",
+        lng: "81.1496",
+      },
+    },
+    phone: "1-770-736-8031 x56442",
+    website: "hildegard.org",
+    company: {
+      name: "Romaguera-Crona",
+      catchPhrase: "Multi-layered client-server neural-net",
+      bs: "harness real-time e-markets",
+    },
+  },
+  {
+    id: 2,
+    name: "Ervin Howell",
+    username: "Antonette",
+    email: "Shanna@melissa.tv",
+    address: {
+      street: "Victor Plains",
+      suite: "Suite 879",
+      city: "Wisokyburgh",
+      zipcode: "90566-7771",
+      geo: {
+        lat: "-43.9509",
+        lng: "-34.4618",
+      },
+    },
+    phone: "010-692-6593 x09125",
+    website: "anastasia.net",
+    company: {
+      name: "Deckow-Crist",
+      catchPhrase: "Proactive didactic contingency",
+      bs: "synergize scalable supply-chains",
+    },
+  },
+];
+
+const sortArray = (arr, sortBy) => {
+  return arr.sort((a, b) => (a[sortBy] > b[sortBy] ? 1 : -1));
+};
